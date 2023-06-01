9321d1b5996abd458a36ce07d2fb09114cf71d1f
zSchloppp <113934308+zSchloppp@users.noreply.github.com>
2023-06-01 12:44:54 -0500

    BDA_EX & BDA_SFX - Fix
    Fixed config for BDA_EX & BDA_SFX

--- Mod/Black_Dagger_Armory/BDA_EX/BDA_Egret/config.cpp
+++ Mod/Black_Dagger_Armory/BDA_EX/BDA_Egret/config.cpp
@@ -2,22 +2,28 @@ class CfgPatches
 {
 	class BDA_EX_Egret
 	{
-        author = "Schlopp";
-        version = "0.02";
-		addonRootClass = "A3_Armor_F_Beta";
-		requiredAddons[] = {"A3_Armor_F_Beta","A3_Data_F","A3_Weapons_F_Gamma", "A3_Sounds_F", "A3_Sounds_F_Mark"};
-		requiredVersion = 0.1;
-		units[] = 
-        {
-            "BDA_Egret_Spaceliner_Static_F",
-            "BDA_Egret_Spaceliner_F"
-        };
-		weapons[] = {};
-		magazines[] = {};
-		ammo[] = {};
+		author="Schlopp";
+		version="0.02";
+		addonRootClass="A3_Armor_F_Beta";
+		requiredAddons[]=
+		{
+			"A3_Armor_F_Beta",
+			"A3_Data_F",
+			"A3_Weapons_F_Gamma",
+			"A3_Sounds_F",
+			"A3_Sounds_F_Mark"
+		};
+		requiredVersion=0.1;
+		units[]=
+		{
+			"BDA_Egret_Spaceliner_Static_F",
+			"BDA_Egret_Spaceliner_F"
+		};
+		weapons[]={};
+		magazines[]={};
+		ammo[]={};
 	};
 };
-
 class cfgVehicles
 {
 	class Thing;
@@ -25,73 +31,94 @@ class cfgVehicles
 	class OPTRE_Pelican_unarmed;
 	class BDA_Egret_Spaceliner_Static_F: Thing
 	{
-		author = "Schlopp";
-		scope = 2;
+		author="Schlopp";
+		scope=2;
 		editorCategory="BDA_EdCat_BlackDagger";
 		editorSubcategory="BDA_EdSubCat_AerospaceShip";
-		armor = 500000;
-		displayname = "Egret Spaceliner";
-		model = "BDA_EX\BDA_Egret\BDA_Egret_Spaceliner";
-		hiddenSelections[] = {"camo"};
-		hiddenSelectionsTextures[] = {"BDA_EX\BDA_Egret\data\BDA_Civilian_Transport_CO.paa"};
+		armor=500000;
+		displayname="Egret Spaceliner";
+		model="BDA_EX\BDA_Egret\BDA_Egret_Spaceliner";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"BDA_EX\BDA_Egret\data\BDA_Civilian_Transport_CO.paa"
+		};
 	};
-
 	class BDA_Egret_Spaceliner_F: OPTRE_Pelican_unarmed
 	{
-		author = "Schlopp";
-		scope = 2;
-		armor = 200000;
-		side = 1;
-		faction = "B_UNSCODST";
+		author="Schlopp";
+		scope=2;
+		armor=200000;
+		side=1;
+		faction="B_UNSCODST";
 		editorSubcategory="BDA_EdSubCat_AerospaceShip";
-		displayname = "Egret Spaceliner";
-		model = "BDA_EX\BDA_Egret\BDA_Egret_Spaceliner";
-		hiddenSelections[] = {"camo"};
-		hiddenSelectionsTextures[] = {"BDA_EX\BDA_Egret\data\BDA_Civilian_Transport_CO.paa"};
+		displayname="Egret Spaceliner";
+		model="BDA_EX\BDA_Egret\BDA_Egret_Spaceliner";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"BDA_EX\BDA_Egret\data\BDA_Civilian_Transport_CO.paa"
+		};
 		soundSetSonicBoom[]=
-        {
-            "Plane_Fighter_SonicBoom_SoundSet"
-        };
-        soundLocked[]=
-        {
-            "BDA_EX\BDA_Egret\sfx\targeted_alarm.ogg",
-            1,
-            1
-        };
-        soundIncommingMissile[]=
-        {
-            "NCA\NCA_vehicles\aircraft\sounds\incomingMissile_alarm.ogg",
-            1,
-            1
-        };
-        soundEngineOnExt[]=
-        {
-            "BDA_EX\BDA_Egret\sfx\engineStartup_exterior.ogg",
-            1.4125376,
-            1,
-            600
-        };
-        soundEngineOnInt[]=
-        {
-            "BDA_EX\BDA_Egret\sfx\engineStartup_interior.ogg",
-            1.4125376,
-            1,
-            600
-        };
-        soundEngineOffExt[]=
-        {
-            "BDA_EX\BDA_Egret\sfx\engineShutdown_exterior.ogg",
-            0.31622776,
-            1
-        };
-        soundEngineOffInt[]=
-        {
-            "BDA_EX\BDA_Egret\sfx\engineShutdown_interior.ogg",
-            0.79432821,
-            1,
-            600
-        };
-        flySound[] = {"BDA_EX\BDA_Egret\sfx\engine_exterior.ogg", 0.000000, 1, 1};
+		{
+			"Plane_Fighter_SonicBoom_SoundSet"
+		};
+		soundLocked[]=
+		{
+			"BDA_EX\BDA_Egret\sfx\targeted_alarm.ogg",
+			1,
+			1
+		};
+		soundIncommingMissile[]=
+		{
+			"NCA\NCA_vehicles\aircraft\sounds\incomingMissile_alarm.ogg",
+			1,
+			1
+		};
+		soundEngineOnExt[]=
+		{
+			"BDA_EX\BDA_Egret\sfx\engineStartup_exterior.ogg",
+			1.4125376,
+			1,
+			600
+		};
+		soundEngineOnInt[]=
+		{
+			"BDA_EX\BDA_Egret\sfx\engineStartup_interior.ogg",
+			1.4125376,
+			1,
+			600
+		};
+		soundEngineOffExt[]=
+		{
+			"BDA_EX\BDA_Egret\sfx\engineShutdown_exterior.ogg",
+			0.31622776,
+			1
+		};
+		soundEngineOffInt[]=
+		{
+			"BDA_EX\BDA_Egret\sfx\engineShutdown_interior.ogg",
+			0.79432821,
+			1,
+			600
+		};
+		flySound[]=
+		{
+			"BDA_EX\BDA_Egret\sfx\engine_exterior.ogg",
+			0,
+			1,
+			1
+		};
 	};
 };
-
+class cfgMods
+{
+	author="Schlopp";
+	timepacked="1680888487";
+};
--- Mod/Black_Dagger_Armory/BDA_SFX/music/config.cpp
+++ Mod/Black_Dagger_Armory/BDA_SFX/music/config.cpp
@@ -2,115 +2,336 @@ class CfgPatches
 {
 	class BDA_Sounds
 	{
-		units[] = {};
-		weapons[] = {};
-		requiredVersion = 0.1;
-		requiredAddons[] = {};
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]={};
 	};
 };
-
 class CfgMusicClasses
 {
 	class BDA_RCH
 	{
-		displayName = "Black Dagger (Reach)";
+		displayName="Black Dagger (Reach)";
 	};
 	class BDA_2
 	{
-		displayName = "Black Dagger (2)";
+		displayName="Black Dagger (2)";
 	};
 	class BDA_3
 	{
-		displayName = "Black Dagger (3)";
+		displayName="Black Dagger (3)";
 	};
 	class BDA_OTH
 	{
-		displayName = "Black Dagger (Other)";
+		displayName="Black Dagger (Other)";
 	};
 };
-
 class CfgMusic
 {
 	tracks[]={};
-	
 	class BDA_MjolnirMix
 	{
-		name = "Mjolnir Mix";
-		sound[] = {"BDA_SFX\music\data\MjolnirMix.ogg",1,1};
-		theme = "Halo 2";
-		duration = "251";
-		musicClass = "BDA_2";
+		name="Mjolnir Mix";
+		sound[]=
+		{
+			"BDA_SFX\music\data\MjolnirMix.ogg",
+			1,
+			1
+		};
+		theme="Halo 2";
+		duration="251";
+		musicClass="BDA_2";
 	};
 	class BDA_WarthogRun
 	{
-		name = "Warthog Run";
-		sound[] = {"BDA_SFX\music\data\WarthogRun.ogg",1,1};
-		theme = "Halo 3";
-		duration = "500";
-		musicClass = "BDA_3";
+		name="Warthog Run";
+		sound[]=
+		{
+			"BDA_SFX\music\data\WarthogRun.ogg",
+			1,
+			1
+		};
+		theme="Halo 3";
+		duration="500";
+		musicClass="BDA_3";
 	};
 	class BDA_Overture
 	{
-		name = "Overture";
-		sound[] = {"BDA_SFX\music\data\Overture.ogg",1,1};
-		theme = "Reach";
-		duration = "287";
-		musicClass = "BDA_RCH";
+		name="Overture";
+		sound[]=
+		{
+			"BDA_SFX\music\data\Overture.ogg",
+			1,
+			1
+		};
+		theme="Reach";
+		duration="287";
+		musicClass="BDA_RCH";
 	};
 	class BDA_WinterContingency
 	{
-		name = "Winter Contingency";
-		sound[] = {"BDA_SFX\music\data\WinterContingency.ogg",1,1};
-		theme = "Reach";
-		duration = "730";
-		musicClass = "BDA_RCH";
+		name="Winter Contingency";
+		sound[]=
+		{
+			"BDA_SFX\music\data\WinterContingency.ogg",
+			1,
+			1
+		};
+		theme="Reach";
+		duration="730";
+		musicClass="BDA_RCH";
 	};
 	class BDA_ONI_SwordBase
 	{
-		name = "ONI: Sword Base";
-		sound[] = {"BDA_SFX\music\data\ONI_SwordBase.ogg",1,1};
-		theme = "Reach";
-		duration = "508";
-		musicClass = "BDA_RCH";
+		name="ONI: Sword Base";
+		sound[]=
+		{
+			"BDA_SFX\music\data\ONI_SwordBase.ogg",
+			1,
+			1
+		};
+		theme="Reach";
+		duration="508";
+		musicClass="BDA_RCH";
 	};
 	class BDA_Nightfall
 	{
-		name = "Nightfall";
-		sound[] = {"BDA_SFX\music\data\Nightfall.ogg",1,1};
-		theme = "Reach";
-		duration = "340";
-		musicClass = "BDA_RCH";
+		name="Nightfall";
+		sound[]=
+		{
+			"BDA_SFX\music\data\Nightfall.ogg",
+			1,
+			1
+		};
+		theme="Reach";
+		duration="340";
+		musicClass="BDA_RCH";
 	};
 	class BDA_TipOfTheSpear
 	{
-		name = "Tip of the Spear";
-		sound[] = {"BDA_SFX\music\data\TipOfTheSpear.ogg",1,1};
-		theme = "Reach";
-		duration = "365";
-		musicClass = "BDA_RCH";
+		name="Tip of the Spear";
+		sound[]=
+		{
+			"BDA_SFX\music\data\TipOfTheSpear.ogg",
+			1,
+			1
+		};
+		theme="Reach";
+		duration="365";
+		musicClass="BDA_RCH";
 	};
 	class BDA_LongNightOfSolace
 	{
-		name = "Long Night of Solace";
-		sound[] = {"BDA_SFX\music\data\LongNightOfSolace.ogg",1,1};
-		theme = "Reach";
-		duration = "706";
-		musicClass = "BDA_RCH";
+		name="Long Night of Solace";
+		sound[]=
+		{
+			"BDA_SFX\music\data\LongNightOfSolace.ogg",
+			1,
+			1
+		};
+		theme="Reach";
+		duration="706";
+		musicClass="BDA_RCH";
 	};
 	class BDA_Exodus
 	{
-		name = "Exodus";
-		sound[] = {"BDA_SFX\music\data\Exodus.ogg",1,1};
-		theme = "Reach";
-		duration = "438";
-		musicClass = "BDA_RCH";
+		name="Exodus";
+		sound[]=
+		{
+			"BDA_SFX\music\data\Exodus.ogg",
+			1,
+			1
+		};
+		theme="Reach";
+		duration="438";
+		musicClass="BDA_RCH";
+	};
+	class BDA_NewAlexandria
+	{
+		name="New Alexandria";
+		sound[]=
+		{
+			"BDA_SFX\music\data\NewAlexandria.ogg",
+			1,
+			1
+		};
+		theme="Reach";
+		duration="504";
+		musicClass="BDA_RCH";
+	};
+	class BDA_ThePackage
+	{
+		name="The Package";
+		sound[]=
+		{
+			"BDA_SFX\music\data\ThePackage.ogg",
+			1,
+			1
+		};
+		theme="Reach";
+		duration="393";
+		musicClass="BDA_RCH";
+	};
+	class BDA_ThePillarofAutumn
+	{
+		name="The Pillar of Autumn";
+		sound[]=
+		{
+			"BDA_SFX\music\data\ThePillarofAutumn.ogg",
+			1,
+			1
+		};
+		theme="Reach";
+		duration="564";
+		musicClass="BDA_RCH";
+	};
+	class BDA_Epilogue
+	{
+		name="Epilogue";
+		sound[]=
+		{
+			"BDA_SFX\music\data\Epilogue.ogg",
+			1,
+			1
+		};
+		theme="Reach";
+		duration="257";
+		musicClass="BDA_RCH";
+	};
+	class BDA_FromtheVault
+	{
+		name="From the Vault";
+		sound[]=
+		{
+			"BDA_SFX\music\data\FromtheVault.ogg",
+			1,
+			1
+		};
+		theme="Reach";
+		duration="275";
+		musicClass="BDA_RCH";
+	};
+	class BDA_Ashes
+	{
+		name="Ashes";
+		sound[]=
+		{
+			"BDA_SFX\music\data\Ashes.ogg",
+			1,
+			1
+		};
+		theme="Reach";
+		duration="147";
+		musicClass="BDA_RCH";
+	};
+	class BDA_Fortress
+	{
+		name="Fortress";
+		sound[]=
+		{
+			"BDA_SFX\music\data\Fortress.ogg",
+			1,
+			1
+		};
+		theme="Reach";
+		duration="64";
+		musicClass="BDA_RCH";
+	};
+	class BDA_WereNotGoingAnywhere
+	{
+		name="We're Not Going Anywhere";
+		sound[]=
+		{
+			"BDA_SFX\music\data\WereNotGoingAnywhere.ogg",
+			1,
+			1
+		};
+		theme="Reach";
+		duration="67";
+		musicClass="BDA_RCH";
+	};
+	class BDA_AtAnyCost
+	{
+		name="At Any Cost";
+		sound[]=
+		{
+			"BDA_SFX\music\data\AtAnyCost.ogg",
+			1,
+			1
+		};
+		theme="Reach";
+		duration="138";
+		musicClass="BDA_RCH";
+	};
+	class BDA_BothWays_Remix
+	{
+		name="Both Ways - Remix";
+		sound[]=
+		{
+			"BDA_SFX\music\data\BothWays_Remix.ogg",
+			1,
+			1
+		};
+		theme="Reach";
+		duration="130";
+		musicClass="BDA_RCH";
+	};
+	class BDA_WalkingAway
+	{
+		name="Walking Away";
+		sound[]=
+		{
+			"BDA_SFX\music\data\WalkingAway.ogg",
+			1,
+			1
+		};
+		theme="Reach";
+		duration="91";
+		musicClass="BDA_RCH";
+	};
+	class BDA_GhostsAndGlass
+	{
+		name="Ghosts and Glass";
+		sound[]=
+		{
+			"BDA_SFX\music\data\GhostsAndGlass.ogg",
+			1,
+			1
+		};
+		theme="Reach";
+		duration="144";
+		musicClass="BDA_RCH";
+	};
+	class BDA_WeRemember
+	{
+		name="We Remember";
+		sound[]=
+		{
+			"BDA_SFX\music\data\WeRemember.ogg",
+			1,
+			1
+		};
+		theme="Reach";
+		duration="123";
+		musicClass="BDA_RCH";
 	};
 	class BDA_FreeBird
 	{
-		name = "Free Bird";
-		sound[] = {"BDA_SFX\music\data\FreeBird.ogg",1,1};
-		theme = "Lynyrd Skynyrd";
-		duration = "550";
-		musicClass = "BDA_OTH";
+		name="Free Bird";
+		sound[]=
+		{
+			"BDA_SFX\music\data\FreeBird.ogg",
+			1,
+			1
+		};
+		theme="Lynyrd Skynyrd";
+		duration="550";
+		musicClass="BDA_OTH";
 	};
+};
+class cfgMods
+{
+	author="Schlopp";
+	timepacked="1683667119";
 };
