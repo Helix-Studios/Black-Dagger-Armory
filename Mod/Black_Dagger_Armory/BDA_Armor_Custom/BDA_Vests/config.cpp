ec07d04698d2b390e70932cecbc04c81a77f3068
SirSmity <gamemaster6484@gmail.com>
2024-03-26 17:45:48 -0400

    Griffin Custom Armor

--- Mod/Black_Dagger_Armory/BDA_Armor_Custom/BDA_Vests/config.cpp
+++ Mod/Black_Dagger_Armory/BDA_Armor_Custom/BDA_Vests/config.cpp
@@ -543,6 +543,17 @@ class cfgWeapons
 		};
 	};
 
+	class BDA_Armor_Griffin: BDA_Armor_MD {
+		author = "Griffin";
+		displayName = "M52D Body Armor (T. Griffin)";
+		hiddenSelectionsTextures[] = {
+			"BDA_Armor\BDA_Vests\data\BDA_Vest_CO.paa",
+			"BDA_Armor\BDA_Vests\data\BDA_Armor_S4_Medic_CO.paa",
+			"BDA_Armor\BDA_Vests\data\BDA_Legs_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co",
+			"BDA_Armor_Custom\BDA_Vests\data\BDA_M52D_Armor_Griffin_CO.paa"
+		};
+	};
 };
 class cfgMods
 {
