13fe3861fed64c52d18ebd553fa95fa0435804e9
Ribbington <plumridgerob@gmail.com>
2024-04-29 00:55:57 +0100

    more AED

--- Other/Scripts/AED/config.cpp
+++ Other/Scripts/AED/config.cpp
@@ -5,7 +5,7 @@ class CfgPatches {
         requiredVersion=0.1;
         version=0.1;
         units[]={
-            "BDA_AED"
+            "BDA_AEDItem"
         };
         weapons[]={
             "BDA_AED"
@@ -29,7 +29,20 @@ class CfgPatches {
 };
 
 class CfgFunctions {
-    class BDA_FunctionYeet {};
+    class BDA_FunctionYeet {
+        tag="ace_medical_treatment";
+        class ace_medical_treatment
+		{
+			class cprLocal
+			{
+				file="\x\bda\addons\circulation\functions\fnc_cprLocal.sqf";
+			};
+			class cprSuccess
+			{
+				file="\x\bda\addons\circulation\functions\fnc_cprSuccess.sqf";
+			};
+		};
+    };
 };
 
 class CfgWeapons {
@@ -39,7 +52,7 @@ class CfgWeapons {
         scope=2;
         author="Rib";
         displayName="Automated External Defibrillator";
-        model="";
+        model="\A3\Structures_F_EPA\Items\Medical\Defibrillator_F.p3d";
         descriptionShort="AED";
         ACE_isMedicalItem=1;
         class ItemInfo: CBA_MiscItem_ItemInfo {
@@ -61,13 +74,13 @@ class CfgVehicles {
 		ace_cargo_canLoad=1;
 	};
     class ThingX;
-    class kat_AEDItem: ThingX {
+    class BDA_AEDItem: ThingX {
         scope=2;
         scopeCurator=2;
         displayName="Automated External Defibrillator";
         author="Rib";
         vehicleClass="Items";
-        model="";
+        model="\A3\Structures_F_EPA\Items\Medical\Defibrillator_F.p3d";
         class TransportItems
 		{
 			class _xx_BDA_AED
@@ -80,11 +93,49 @@ class CfgVehicles {
 		ace_dragging_canCarry=1;
 		ace_dragging_dragPosition[]={0,1.1,0};
 		ace_dragging_carryPosition[]={0,1.1,1};
-        class ACE_Actions {
-            class ACE_MainActions {
-                
-            };
-        };
+    };
+};
+
+class ACE_Medical_Treatment_Actions {
+    class BasicBandage;
+    class CPR: BasicBandage {
+        displayName = "$STR_ACE_Medical_Treatment_Actions_CPR";
+        displayNameProgress = "$STR_ACE_Medical_Treatment_Actions_PerformingCPR";
+        icon = "";
+        category = "advanced";
+        treatmentLocations = TREATMENT_LOCATIONS_ALL;
+        allowedSelections[] = {"Body"};
+        allowSelfTreatment = 0;
+        medicRequired = 0;
+        treatmentTime = 15;
+        items[] = {};
+        condition = QFUNC(canCPR);
+        callbackSuccess = QFUNC(cprSuccess);
+        callbackFailure = QFUNC(cprFailure);
+        callbackProgress = QFUNC(cprProgress);
+        callbackStart = QFUNC(cprStart);
+        animationMedic = "AinvPknlMstpSnonWnonDr_medic0";
+        animationMedicProne = "AinvPknlMstpSnonWnonDr_medic0";
+        animationMedicSelf = "";
+        animationMedicSelfProne = "";
+        consumeItem = 0;
+        litter[] = {};
+    };
+
+    class Defibrillator: CPR {
+        displayName = "$STR_BDA_Medical_Treatment_Actions_Defib";
+        displayNameProgress = "$STR_BDA_Medical_Treatment_Actions_PerformingDefib";
+        icon = "\BDA_AED\ui\defib_action.paa";
+        items[] = {"BDA_AED"};
+        consumeItem = 0;
+        condition = QFUNC(canCPR);
+		animationMedic = "AinvPknlMstpSnonWnonDnon_medic3";
+        animationMedicProne = "AinvPknlMstpSnonWnonDr_medic0";
+        treatmentTime = 10;
+        callbackSuccess = QFUNC(cprSuccess);
+        callbackFailure = QFUNC(cprFailure);
+        callbackProgress = QFUNC(cprProgress);
+        callbackStart = QFUNC(cprStart);
     };
 };
     
