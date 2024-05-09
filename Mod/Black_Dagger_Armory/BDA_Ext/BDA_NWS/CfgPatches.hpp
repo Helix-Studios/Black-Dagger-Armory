cab1e6c8326569b3e07893a3b27b6a9bbc4148b9
Ribbington <plumridgerob@gmail.com>
2024-05-09 22:22:24 +0100

    No weapon sway

---
+++ Mod/Black_Dagger_Armory/BDA_Ext/BDA_NWS/CfgPatches.hpp
@@ -0,0 +1,12 @@
+class CfgPatches
+{
+	class BDA_NWS
+	{
+		name = "No Weapon Sway";
+		author = "Rib";
+		requiredVersion = 1.0;
+		requiredAddons[] = {"A3_Data_F"};
+		units[] = {};
+		weapons[] = {};
+	};
+};
