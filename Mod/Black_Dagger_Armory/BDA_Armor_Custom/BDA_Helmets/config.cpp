a8fcf8e270ab3dbfa369de0816175030244bd1b1
Ribbington <plumridgerob@gmail.com>
2024-06-16 03:23:25 +0100

    boston visor

--- Mod/Black_Dagger_Armory/BDA_Armor_Custom/BDA_Helmets/config.cpp
+++ Mod/Black_Dagger_Armory/BDA_Armor_Custom/BDA_Helmets/config.cpp
@@ -793,7 +793,7 @@ class cfgWeapons
 		hiddenSelectionsTextures[]=
 		{
 			"BDA_Armor_Custom\BDA_Helmets\data\BDA_Helmet_Boston_CO.paa",
-			"BDA_Armor\BDA_Helmets\data\visors\BDA_Visor_Scythe_CO.paa",
+			"BDA_Armor\BDA_Helmets\data\visors\BDA_Visor_Boston_CO.paa",
 			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
 			"BDA_Armor\BDA_Helmets\data\BDA_Soft_Packs_co.paa"
 		};
@@ -810,7 +810,7 @@ class cfgWeapons
 			hiddenSelectionsTextures[]=
 			{
 				"BDA_Armor_Custom\BDA_Helmets\data\BDA_Helmet_Boston_CO.paa",
-				"BDA_Armor\BDA_Helmets\data\visors\BDA_Visor_Scythe_CO.paa",
+				"BDA_Armor\BDA_Helmets\data\visors\BDA_Visor_Boston_CO.paa",
 				"optre_unsc_units\army\data\ghillie_woodland_co.paa",
 				"BDA_Armor\BDA_Helmets\data\BDA_Soft_Packs_co.paa"
 			};
