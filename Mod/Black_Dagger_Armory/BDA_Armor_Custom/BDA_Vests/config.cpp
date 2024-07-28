66d2fc8d483713d04b5276041df9dce8b4575705
Ribbington <plumridgerob@gmail.com>
2024-07-28 22:52:58 +0100

    sniper removal, karson white

--- Mod/Black_Dagger_Armory/BDA_Armor_Custom/BDA_Vests/config.cpp
+++ Mod/Black_Dagger_Armory/BDA_Armor_Custom/BDA_Vests/config.cpp
@@ -734,18 +734,6 @@ class cfgWeapons
 		};
 	};
 
-	class BDA_Armor_Vito: BDA_Armor_AR {
-		author = "Rib";
-		displayName = "M52D Body Armor (D. Vito)";
-		hiddenSelectionsTextures[]= {
-			"BDA_Armor\BDA_Vests\data\BDA_Vest_CO.paa",
-			"BDA_Armor\BDA_Vests\data\BDA_Armor_CO.paa",
-			"BDA_Armor\BDA_Legs\data\BDA_Legs_CO.paa",
-			"optre_unsc_units\army\data\ghillie_woodland_co",
-			"BDA_Armor_Custom\BDA_Vests\data\BDA_M52D_Armor_Sniper_CO.paa"
-		};
-	};
-
 	class BDA_Armor_Beefe: BDA_Armor_RF {
 		author = "Grif";
 		displayName = "M52D Body Armor (G. Beefe)";
