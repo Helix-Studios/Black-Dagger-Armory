6df6c1123e346ae79f35453c9df3809c4968a944
SirSmity <gamemaster6484@gmail.com>
2024-08-25 18:36:27 -0400

    Wolsey Helm/Armor

--- Mod/Black_Dagger_Armory/BDA_Armor_Custom/BDA_Vests/config.cpp
+++ Mod/Black_Dagger_Armory/BDA_Armor_Custom/BDA_Vests/config.cpp
@@ -849,6 +849,18 @@ class cfgWeapons
 			"BDA_Armor_Custom\BDA_Vests\data\BDA_M52D_Armor_Parhau_CO.paa"
 		};
 	};
+
+	class BDA_Armor_Wolsey: BDA_Armor_RF {
+		author = "Grif";
+		displayName = "M52D Body Armor (T. Wolsey)";
+		hiddenSelectionsTextures[]= {
+			"BDA_Armor\BDA_Vests\data\BDA_Vest_CO.paa",
+			"BDA_Armor\BDA_Vests\data\BDA_Armor_CO.paa",
+			"BDA_Armor\BDA_Legs\data\BDA_Legs_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co",
+			"BDA_Armor_Custom\BDA_Vests\data\BDA_M52D_Armor_Wolsey_CO.paa"
+		};
+	};
 	//end
 };
 class cfgMods
