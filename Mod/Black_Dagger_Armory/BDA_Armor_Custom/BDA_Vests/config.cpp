1d11a9fa0a3df7d6645f329348a0e67450615b7f
SirSmity <gamemaster6484@gmail.com>
2024-10-27 00:20:23 -0400

    Bomb Helmet and Armor

--- Mod/Black_Dagger_Armory/BDA_Armor_Custom/BDA_Vests/config.cpp
+++ Mod/Black_Dagger_Armory/BDA_Armor_Custom/BDA_Vests/config.cpp
@@ -900,6 +900,17 @@ class cfgWeapons
 		};
 	};
 
+	class BDA_Armor_Bomb: BDA_Armor_BR {
+		author = "Grif";
+		displayName = "M52D Body Armor (J. Bomb)";
+		hiddenSelectionsTextures[]= {
+			"BDA_Armor\BDA_Vests\data\BDA_Vest_CO.paa",
+			"BDA_Armor\BDA_Vests\data\BDA_Armor_CO.paa",
+			"BDA_Armor\BDA_Legs\data\BDA_Legs_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co",
+			"BDA_Armor_Custom\BDA_Vests\data\BDA_M52D_Armor_Bomb_CO.paa"
+		};
+	};
 	//end
 };
 
