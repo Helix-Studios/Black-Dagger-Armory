4b326d12518900e72d24c5fa31d0335715f12d1d
SirSmity <gamemaster6484@gmail.com>
2024-12-17 19:16:32 -0500

    Conrad Armor

--- Mod/Black_Dagger_Armory/BDA_Armor_Custom/BDA_Vests/config.cpp
+++ Mod/Black_Dagger_Armory/BDA_Armor_Custom/BDA_Vests/config.cpp
@@ -822,7 +822,7 @@ class cfgWeapons {
 		};
 	};
 	
-	class BDA_Armor_Conrad: BDA_Armor_AR {
+	class BDA_Armor_Conrad: BDA_Armor_RF {
 		author = "Grif";
 		displayName = "M52D Body Armor (A. Conrad)";
 		hiddenSelectionsTextures[]= {
