74c88fdf265151093deceaa21d09480ddc7e7f4c
Ribbington_v2 <plumridgerob@gmail.com>
2025-01-24 20:32:11 +0000

    BDA_1TA init

---
+++ Source/Textures/Factions/1ITA/BDA_1ITA/CfgPatches.hpp
@@ -0,0 +1,38 @@
+class CfgPatches {
+    class O_1ITA {
+        units[] = {
+            "O_1ITA_SquadLeader",
+            "O_1ITA_Sniper",
+            "O_1ITA_RadioOperator",
+            "O_1ITA_Rifleman",
+            "O_1ITA_Officer",
+            "O_1ITA_Heavy_Gunner",
+            "O_1ITA_Autorifleman",
+            "O_1ITA_AntiTankSpecialist",
+            "O_1ITA_CombatMedic",
+            "O_1ITA_CombatCrewman",
+            "O_1ITA_Heli_Pilot",
+            "O_1ITA_JetPilot"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "A3_Characters_F",
+            "kka3_gestures_ace",
+            "A3_Sounds_F",
+            "A3_Sounds_F_Exp",
+            "ace_movement",
+            "ace_medical_feedback",
+            "A3_Data_F",
+            "A3_Data_F_Curator",
+            "A3_Air_F_Heli",
+            "ace_thermals",
+            "zen_attributes",
+            "ace_refuel",
+            "CUP_Core",
+            "cba_xeh"
+        };
+        author = "1stLt. R. Ribbington";
+        authors[] = {"1stLt. R. Ribbington"};
+    };
+};
---
+++ Source/Textures/Factions/1ITA/BDA_1ITA/script_component.hpp
@@ -0,0 +1,16 @@
+#define COMPONENT o1ita
+#define COMPONENT_BEAUTIFIED o1ita
+#include "\x\\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_O1ITA
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_O1ITA
+#define DEBUG_SETTINGS DEBUG_SETTINGS_O1ITA
+#endif
+#include "\x\\addons\main\script_macros.hpp"
+#define COMPONENT o1ita
+#define COMPONENT_BEAUTIFIED o1ita
+#include "\x\\addons\main\script_mod.hpp"
