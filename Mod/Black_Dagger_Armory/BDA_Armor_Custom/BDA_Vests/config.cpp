4e131906b6680d1f01300edc27fa441bc249fd12
SirSmity <gamemaster6484@gmail.com>
2025-02-17 16:10:14 -0500

    Kohl Armor and Helmet

--- Mod/Black_Dagger_Armory/BDA_Armor_Custom/BDA_Vests/config.cpp
+++ Mod/Black_Dagger_Armory/BDA_Armor_Custom/BDA_Vests/config.cpp
@@ -810,6 +810,20 @@ class cfgWeapons {
 		};
 	};
 
+	class BDA_Armor_Kohl: BDA_Armor_RF {
+		dlc="BDA";
+		author = "Grif";
+		picture="\BDA_Armor\BDA_Vests\data\icons\BDA_ODST_Armor";
+		displayName="M52D Body Armor (R. Kohl)";
+		hiddenSelectionsTextures[]= {
+			"BDA_Armor\BDA_Vests\data\BDA_Vest_CO.paa",
+			"BDA_Armor\BDA_Vests\data\BDA_Armor_CO.paa",
+			"BDA_Armor\BDA_Legs\data\BDA_Legs_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co",
+			"BDA_Armor_Custom\BDA_Vests\data\BDA_M52D_Armor_Kohl_CO.paa"
+		};
+	};
+
 	class BDA_Armor_Zodiac: BDA_Armor_MD {
 		author = "Rib";
 		displayName = "M52D Body Armor (A. Zodiac)";
