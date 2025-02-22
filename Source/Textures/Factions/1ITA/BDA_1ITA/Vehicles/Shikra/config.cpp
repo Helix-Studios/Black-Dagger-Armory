ecb955dc2c5bab827b6f2af043fed4a09eeda0f1
Ribbington_v2 <plumridgerob@gmail.com>
2025-02-22 15:14:28 +0000

    shikra textures and code completion

--- Source/Textures/Factions/1ITA/BDA_1ITA/Vehicles/Shikra/config.cpp
+++ Source/Textures/Factions/1ITA/BDA_1ITA/Vehicles/Shikra/config.cpp
@@ -40,21 +40,21 @@ class CfgVehicles {
         DLC = "BDA";
         faction = "O_1ITA";
         crew = "O_1ITA_JetPilot";
-        typicalCargo = {"O_1ITA_JetPilot"};
-        armor = 120;
+        typicalCargo[] = {"O_1ITA_JetPilot"};
+        armor = 140;
 
 
         hiddenSelectionsTextures[] = {
-            "a3\air_f_jets\plane_fighter_02\data\Fighter_02_fuselage_01_co.paa", 
-            "a3\air_f_jets\plane_fighter_02\data\Fighter_02_fuselage_02_co.paa", 
-            "a3\air_f_jets\plane_fighter_02\data\Fighter_02_fuselage_01_co.paa", 
+            "a3\air_f_jets\plane_fighter_02\data\Fighter_02_fuselage_01_co.paa", //change this
+            "a3\air_f_jets\plane_fighter_02\data\Fighter_02_fuselage_02_co.paa", //and this
+            "a3\air_f_jets\plane_fighter_02\data\Fighter_02_fuselage_01_co.paa", //and this
             "a3\air_f_jets\plane_fighter_02\data\Numbers\Fighter_02_number_02_co.paa", 
             "a3\air_f_jets\plane_fighter_02\data\Numbers\Fighter_02_number_01_co.paa", 
             "a3\air_f_jets\plane_fighter_02\data\Numbers\Fighter_02_number_04_co.paa"
         };
 
-        weapons = {"weapon_Fighter_Gun_30mm", "Laserdesignator_pilotCamera", "CMFlareLauncher"};
-		magazines = {"magazine_Fighter02_Gun30mm_AA_x180", "Laserbatteries", "240Rnd_CMFlare_Chaff_Magazine", "240Rnd_CMFlare_Chaff_Magazine"};
+        weapons[] = {"weapon_Fighter_Gun_30mm", "Laserdesignator_pilotCamera", "CMFlareLauncher"};
+		magazines[] = {"magazine_Fighter02_Gun30mm_AA_x180", "magazine_Fighter02_Gun30mm_AA_x180", "Laserbatteries", "240Rnd_CMFlare_Chaff_Magazine", "240Rnd_CMFlare_Chaff_Magazine"};
         class Components: Components {
 			class SensorsManagerComponent {
 				class Components {
@@ -159,7 +159,7 @@ class CfgVehicles {
 					};
 					class SensorDisplay {
 						componentType = "SensorsDisplayComponent";
-						range = {16000, 8000, 4000, 2000};
+						range[] = {16000, 8000, 4000, 2000};
 						resource = "RscCustomInfoSensors";
 					};
 				};
@@ -187,7 +187,7 @@ class CfgVehicles {
 					};
 					class SensorDisplay {
 						componentType = "SensorsDisplayComponent";
-						range = {16000, 8000, 4000, 2000};
+						range[] = {16000, 8000, 4000, 2000};
 						resource = "RscCustomInfoSensors";
 					};
 				};
@@ -196,44 +196,87 @@ class CfgVehicles {
 				UIPicture = "\A3\Air_F_Jets\Plane_Fighter_02\Data\UI\Fighter_02_3DEN_ca.paa";
 				class pylons {
 					class pylons1 {
-						hardpoints = {"O_R73", "O_R77"};
+						hardpoints[] = {"O_R73", 
+							"O_R77", 
+							"O_MISSILE_PYLON",
+							"O_BOMB_PYLON_HELI",
+							"OPAEX_Hardpoint_AV22",
+							"OPAEX_Hardpoint_D77-TC",
+							"OPAEX_Hardpoint_AV14",
+							"O_KH25", 
+							"O_KAB250_BOMB", 
+							"O_KH58"
+						};
 						attachment = "PylonMissile_Missile_AA_R73_x1";
 						priority = 13;
 						maxweight = 300;
-						UIposition = {0.600000, 0.450000};
+						UIposition[] = {0.600000, 0.450000};
 					};
 					class pylons2: pylons1 {
-						UIposition = {0.050000, 0.450000};
+						UIposition[] = {0.050000, 0.450000};
 						mirroredMissilePos = 1;
 					};
 					class pylons3 {
-						hardpoints = {"O_R73", "O_R77", "O_KH25", "O_KAB250_BOMB"};
+						hardpoints[] = {
+							"O_R73", 
+							"O_R77", 
+							"O_MISSILE_PYLON",
+							"O_BOMB_PYLON_HELI",
+							"OPAEX_Hardpoint_AV22",
+							"OPAEX_Hardpoint_D77-TC",
+							"OPAEX_Hardpoint_AV14",
+							"O_KH25", 
+							"O_KAB250_BOMB", 
+							"O_KH58"
+						};
 						attachment = "PylonMissile_Missile_AA_R77_x1";
 						priority = 11;
 						maxweight = 1050;
-						UIposition = {0.550000, 0.350000};
+						UIposition[] = {0.550000, 0.350000};
 					};
 					class pylons4: pylons3 {
-						UIposition = {0.100000, 0.350000};
+						UIposition[] = {0.100000, 0.350000};
 						mirroredMissilePos = 3;
 					};
 					class pylons5 {
-						hardpoints = {"O_R73", "O_R77", "O_KH25", "O_KAB250_BOMB", "O_KH58"};
+						hardpoints[] = {
+							"O_R73", 
+							"O_R77", 
+							"O_MISSILE_PYLON",
+							"O_BOMB_PYLON_HELI",
+							"OPAEX_Hardpoint_AV22",
+							"OPAEX_Hardpoint_D77-TC",
+							"OPAEX_Hardpoint_AV14",
+							"O_KH25", 
+							"O_KAB250_BOMB", 
+							"O_KH58"
+						};
 						attachment = "PylonMissile_Bomb_KAB250_x1";
 						priority = 9;
 						maxweight = 1200;
-						UIposition = {0.500000, 0.250000};
+						UIposition[] = {0.500000, 0.250000};
 					};
 					class pylons6: pylons5 {
-						UIposition = {0.150000, 0.250000};
+						UIposition[] = {0.150000, 0.250000};
 						mirroredMissilePos = 5;
 					};
 					class pylonBayRight1 {
-						hardpoints = {"O_R73"};
+						hardpoints[] = {
+							"O_R73", 
+							"O_R77", 
+							"O_MISSILE_PYLON",
+							"O_BOMB_PYLON_HELI",
+							"OPAEX_Hardpoint_AV22",
+							"OPAEX_Hardpoint_D77-TC",
+							"OPAEX_Hardpoint_AV14",
+							"O_KH25", 
+							"O_KAB250_BOMB", 
+							"O_KH58"
+						};
 						priority = 7;
 						attachment = "PylonMissile_Missile_AA_R73_x1";
 						maxweight = 300;
-						UIposition = {0.450000, 0.150000};
+						UIposition[] = {0.450000, 0.150000};
 						bay = 2;
 					};
 					class pylonBayLeft1: pylonBayRight1 {
@@ -242,36 +285,69 @@ class CfgVehicles {
 						bay = 1;
 					};
 					class pylonBayRight2 {
-						hardpoints = {"O_R77"};
+						hardpoints[] = {
+							"O_R73", 
+							"O_R77", 
+							"O_MISSILE_PYLON",
+							"O_BOMB_PYLON_HELI",
+							"OPAEX_Hardpoint_AV22",
+							"OPAEX_Hardpoint_D77-TC",
+							"OPAEX_Hardpoint_AV14",
+							"O_KH25", 
+							"O_KAB250_BOMB", 
+							"O_KH58"
+						};
 						priority = 5;
 						attachment = "PylonMissile_Missile_AA_R77_x1";
 						maxweight = 750;
-						UIposition = {0.330000, 0.300000};
+						UIposition[] = {0.330000, 0.300000};
 						bay = 4;
 					};
 					class pylonBayLeft2: pylonBayRight2 {
-						UIposition = {0.330000, 0.350000};
+						UIposition[] = {0.330000, 0.350000};
 						mirroredMissilePos = 9;
 						bay = 3;
 					};
 					class pylonBayCenter1 {
-						hardpoints = {"O_R77_INT"};
+						hardpoints[] = {
+							"O_R73", 
+							"O_R77_INT", 
+							"O_MISSILE_PYLON",
+							"O_BOMB_PYLON_HELI",
+							"OPAEX_Hardpoint_AV22",
+							"OPAEX_Hardpoint_D77-TC",
+							"OPAEX_Hardpoint_AV14",
+							"O_KH25_INT", 
+							"O_KAB250_BOMB", 
+							"O_KH58_INT"
+						};
 						priority = 2;
 						attachment = "PylonMissile_Missile_AA_R77_INT_x1";
 						maxweight = 750;
-						UIposition = {0.330000, 0.450000};
+						UIposition[] = {0.330000, 0.450000};
 						bay = 5;
 					};
 					class pylonBayCenter2: pylonBayCenter1 {
-						UIposition = {0.330000, 0.500000};
+						UIposition[] = {0.330000, 0.500000};
 						mirroredMissilePos = 11;
 					};
 					class pylonBayCenter3 {
-						hardpoints = {"O_R77_INT", "O_KH25_INT", "O_KAB250_BOMB", "O_KH58_INT"};
+						hardpoints[] = {
+							"O_R73", 
+							"O_R77_INT", 
+							"O_MISSILE_PYLON",
+							"O_BOMB_PYLON_HELI",
+							"OPAEX_Hardpoint_AV22",
+							"OPAEX_Hardpoint_D77-TC",
+							"OPAEX_Hardpoint_AV14",
+							"O_KH25_INT", 
+							"O_KAB250_BOMB", 
+							"O_KH58_INT"
+						};
 						priority = 1;
 						attachment = "PylonMissile_Bomb_KAB250_x1";
 						maxweight = 1200;
-						UIposition = {0.330000, 0.550000};
+						UIposition[] = {0.330000, 0.550000};
 						bay = 5;
 					};
 				};
@@ -307,19 +383,19 @@ class CfgVehicles {
 				class Presets {
 					class Empty {
 						displayName = "$STR_empty";
-						attachment = {};
+						attachment[] = {};
 					};
 					class Default {
 						displayName = "$STR_vehicle_default";
-						attachment = {"PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Bomb_KAB250_x1", "PylonMissile_Bomb_KAB250_x1", "PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Missile_AA_R77_INT_x1", "PylonMissile_Missile_AA_R77_INT_x1", "PylonMissile_Bomb_KAB250_x1"};
+						attachment[] = {"PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Bomb_KAB250_x1", "PylonMissile_Bomb_KAB250_x1", "PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Missile_AA_R77_INT_x1", "PylonMissile_Missile_AA_R77_INT_x1", "PylonMissile_Bomb_KAB250_x1"};
 					};
 					class AA {
 						displayName = "$STR_A3_cfgmagazines_titan_aa_dns";
-						attachment = {"PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Missile_AA_R77_INT_x1", "PylonMissile_Missile_AA_R77_INT_x1", "PylonMissile_Missile_AA_R77_INT_x1"};
+						attachment[] = {"PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Missile_AA_R77_INT_x1", "PylonMissile_Missile_AA_R77_INT_x1", "PylonMissile_Missile_AA_R77_INT_x1"};
 					};
 					class CAS {
 						displayName = "$STR_A3_CAS_PRESET_DISPLAYNAME";
-						attachment = {"PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AGM_KH25_x1", "PylonMissile_Missile_AGM_KH25_x1", "PylonMissile_Missile_AGM_KH25_x1", "PylonMissile_Missile_AGM_KH25_x1", "PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Missile_AA_R77_INT_x1", "PylonMissile_Missile_AA_R77_INT_x1", "PylonMissile_Missile_AGM_KH25_INT_x1"};
+						attachment[] = {"PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AGM_KH25_x1", "PylonMissile_Missile_AGM_KH25_x1", "PylonMissile_Missile_AGM_KH25_x1", "PylonMissile_Missile_AGM_KH25_x1", "PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AA_R73_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Missile_AA_R77_x1", "PylonMissile_Missile_AA_R77_INT_x1", "PylonMissile_Missile_AA_R77_INT_x1", "PylonMissile_Missile_AGM_KH25_INT_x1"};
 					};
 				};
 			};
