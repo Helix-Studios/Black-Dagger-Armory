ff1cebda92d12a7fe4d70b653a20d6f35d3a7c4b
SirSmity <gamemaster6484@gmail.com>
2025-03-06 20:07:41 -0500

    Reynolds Armor and Helmet

--- Mod/Black_Dagger_Armory/BDA_Armor_Custom/BDA_Vests/config.cpp
+++ Mod/Black_Dagger_Armory/BDA_Armor_Custom/BDA_Vests/config.cpp
@@ -284,9 +284,9 @@ class cfgWeapons {
 				"AS_SmallRight",
 				"AS_SmallLeft",
 				"AP_AR",
-	//			"AP_BR", 
+				"AP_BR", 
 				"AP_Canteen",
-				"AP_GL",
+	//			"AP_GL",
 	//			"AP_Knife",
 				"AP_MGThigh",
 				"AP_AR",
@@ -299,12 +299,12 @@ class cfgWeapons {
 	//			"AP_Thigh",
 	//			"AP_Frag",
 	//			"AP_Smoke",
-	//			"APO_AR",
+				"APO_AR",
 				"APO_BR",
 				"APO_Knife",
 				"APO_SMG",
 				"APO_Sniper",
-				"CustomKit_Scorch"
+	//			"CustomKit_Scorch"
 			};
 			hiddenSelectionsTextures[]={
 				"BDA_Armor\BDA_Vests\data\BDA_Vest_CO.paa",
@@ -687,6 +687,80 @@ class cfgWeapons {
 		};
 	};
 
+	class BDA_Armor_Reynolds: OPTRE_UNSC_M52D_Armor {
+		dlc="BDA";
+		author="Grif";
+		displayName="M52D Body Armor (F. Reynolds)";
+		picture="\BDA_Armor\BDA_Vests\data\icons\BDA_ODST_Armor";
+		hiddenSelectionsTextures[]= {
+			"BDA_Armor\BDA_Vests\data\BDA_Vest_CO.paa",
+			"BDA_Armor\BDA_Vests\data\BDA_Armor_CO.paa",
+			"BDA_Armor\BDA_Legs\data\BDA_Legs_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co",
+			"BDA_Armor_Custom\BDA_Vests\data\BDA_M52D_Armor_Reynolds_CO.paa"
+		};
+		class ItemInfo: ItemInfo {
+				hiddenSelections[]= {
+				"camo",
+				"camo2",
+				"camo3",
+				"camo4",
+				"camo5",
+				"A_Ghillie",
+	//			"A_KneesLeft",
+	//			"A_KneesRight",
+				"A_KneesMarLeft",
+				"A_KneesMarRight",
+	//			"A_ODST",
+	//			"A_TacPad",
+	//			"A_ShinArmorLeft",
+	//			"A_ShinArmorRight",
+	//			"AS_BaseLeft",
+	//			"AS_BaseRight",
+				"AS_LargeLeft",
+				"AS_LargeRight",
+				"AS_MediumLeft",
+				"AS_MediumRight",
+				"AS_ODSTLeft",
+				"AS_ODSTRight",
+	//			"AS_ODSTCQBLeft",
+				"AS_ODSTCQBRight",
+				"AS_ODSTSniperRight",
+				"AS_ODSTSniperLeft",
+				"AS_SmallLeft",
+	//			"AS_SmallRight",
+	//			"AP_AR",
+				"AP_BR",
+				"AP_Canteen",
+				"AP_GL",
+				"AP_Knife",
+				"AP_MGThigh",
+				"AP_AR",
+				"AP_Pack",
+				"AP_Pistol",
+				"AP_Rounds",
+				"AP_SG",
+				"AP_SMG",
+				"AP_Sniper",
+	//			"AP_Thigh",
+	//			"AP_Smoke",
+				"APO_AR",
+				"APO_BR",
+				"APO_Knife",
+				"APO_SMG",
+				"APO_Sniper",
+				"CustomKit_Scorch"
+			};
+			hiddenSelectionsTextures[]= {
+				"BDA_Armor\BDA_Vests\data\BDA_Vest_CO.paa",
+				"BDA_Armor\BDA_Vests\data\BDA_Armor_CO.paa",
+				"BDA_Armor\BDA_Legs\data\BDA_Legs_CO.paa",
+				"optre_unsc_units\army\data\ghillie_woodland_co",
+				"BDA_Armor_Custom\BDA_Vests\data\BDA_M52D_Armor_Reynolds_CO.paa"
+			};
+		};
+	};
+
 	/*class BDA_Armor_Ryder: BDA_Armor_RF {
 		author = "Rib";
 		displayName = "M52D Body Armor (C. Ryder)";
@@ -793,20 +867,6 @@ class cfgWeapons {
 		displayName="M52B Body Armor (R. Parhau)";
 		hiddenSelectionsTextures[]= {
 			"BDA_Armor_Custom\BDA_Vests\data\BDA_M52B_Armor_Parhau_CO.paa" 
-		};
-	};
-
-	class BDA_Armor_Reynolds: OPTRE_UNSC_M52D_Armor_Stripes {
-		dlc="BDA";
-		author = "Rib";
-		picture="\BDA_Armor\BDA_Vests\data\icons\BDA_ODST_Armor";
-		displayName="M52D Body Armor (F. Reynolds)";
-		hiddenSelectionsTextures[]= {
-			"BDA_Armor\BDA_Vests\data\BDA_Vest_CO.paa",
-			"BDA_Armor\BDA_Vests\data\BDA_Armor_CO.paa",
-			"BDA_Armor\BDA_Legs\data\BDA_Legs_CO.paa",
-			"optre_unsc_units\army\data\ghillie_woodland_co",
-			"BDA_Armor_Custom\BDA_Vests\data\BDA_M52D_Armor_Reynolds_CO.paa"
 		};
 	};
 
